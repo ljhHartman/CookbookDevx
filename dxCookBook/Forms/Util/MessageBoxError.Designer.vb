c16d390ba23221f65b8450f1349d87c00c47de7c
Lucas Hartman <lucas.hartman@burando.eu>
2022-08-26 10:48:08 +0200

    26/08/2022: 1. FINSHED: MessgeBoxError

--- dxCookBook/Forms/Util/MessageBoxError.Designer.vb
+++ dxCookBook/Forms/Util/MessageBoxError.Designer.vb
@@ -22,36 +22,78 @@ Partial Class MessageBoxError
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.Button1 = New System.Windows.Forms.Button()
-        Me.DxErrorProvider1 = New DevExpress.XtraEditors.DXErrorProvider.DXErrorProvider()
+        Me.components = New System.ComponentModel.Container()
+        Me.btnRelactionCode = New System.Windows.Forms.Button()
+        Me.DxErrorProvider1 = New DevExpress.XtraEditors.DXErrorProvider.DXErrorProvider(Me.components)
+        Me.teRelationCode = New DevExpress.XtraEditors.TextEdit()
+        Me.lblRelationCode = New System.Windows.Forms.Label()
+        Me.BehaviorManager1 = New DevExpress.Utils.Behaviors.BehaviorManager(Me.components)
+        Me.btnSave = New System.Windows.Forms.Button()
         CType(Me.DxErrorProvider1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.teRelationCode.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.BehaviorManager1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
-        'Button1
+        'btnRelactionCode
         '
-        Me.Button1.Location = New System.Drawing.Point(352, 196)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 23)
-        Me.Button1.TabIndex = 0
-        Me.Button1.Text = "Button1"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.btnRelactionCode.Location = New System.Drawing.Point(341, 12)
+        Me.btnRelactionCode.Name = "btnRelactionCode"
+        Me.btnRelactionCode.Size = New System.Drawing.Size(112, 23)
+        Me.btnRelactionCode.TabIndex = 0
+        Me.btnRelactionCode.Text = "New Relation Code"
+        Me.btnRelactionCode.UseVisualStyleBackColor = True
         '
         'DxErrorProvider1
         '
         Me.DxErrorProvider1.ContainerControl = Me
+        '
+        'teRelationCode
+        '
+        Me.teRelationCode.Location = New System.Drawing.Point(341, 158)
+        Me.teRelationCode.Name = "teRelationCode"
+        Me.teRelationCode.Size = New System.Drawing.Size(112, 20)
+        Me.teRelationCode.TabIndex = 1
+        '
+        'lblRelationCode
+        '
+        Me.lblRelationCode.AutoSize = True
+        Me.lblRelationCode.Location = New System.Drawing.Point(254, 161)
+        Me.lblRelationCode.Name = "lblRelationCode"
+        Me.lblRelationCode.Size = New System.Drawing.Size(70, 13)
+        Me.lblRelationCode.TabIndex = 2
+        Me.lblRelationCode.Text = "Relationcode"
+        '
+        'btnSave
+        '
+        Me.btnSave.Location = New System.Drawing.Point(667, 399)
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(75, 23)
+        Me.btnSave.TabIndex = 3
+        Me.btnSave.Text = "Save"
+        Me.btnSave.UseVisualStyleBackColor = True
         '
         'MessageBoxError
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.btnSave)
+        Me.Controls.Add(Me.lblRelationCode)
+        Me.Controls.Add(Me.teRelationCode)
+        Me.Controls.Add(Me.btnRelactionCode)
         Me.Name = "MessageBoxError"
         Me.Text = "MessageBoxError"
         CType(Me.DxErrorProvider1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.teRelationCode.Properties, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.BehaviorManager1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
-    Friend WithEvents Button1 As Button
-    Friend WithEvents DxErrorProvider1 As DevExpress.XtraEditors.DXErrorProvider.DXErrorProvider
+    Friend WithEvents btnRelactionCode As Button
+    Private WithEvents DxErrorProvider1 As DevExpress.XtraEditors.DXErrorProvider.DXErrorProvider
+    Friend WithEvents lblRelationCode As Label
+    Friend WithEvents teRelationCode As DevExpress.XtraEditors.TextEdit
+    Friend WithEvents BehaviorManager1 As DevExpress.Utils.Behaviors.BehaviorManager
+    Friend WithEvents btnSave As Button
 End Class
--- dxCookBook/Forms/Util/MessageBoxError.vb
+++ dxCookBook/Forms/Util/MessageBoxError.vb
@@ -3,7 +3,48 @@ Imports DevExpress.XtraEditors.DXErrorProvider
 
 Public Class MessageBoxError
 
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+    Sub New()
+        InitializeComponent()
+        AddHandler btnSave.Click, AddressOf btnSave_Click
+        AddHandler btnRelactionCode.Click, AddressOf btnRelactionCode_Click
+    End Sub
+
+
+#Region "Button"
+
+    Private Sub btnSave_Click(sender As Object, e As EventArgs)
+        ValidateEditBox()
+    End Sub
+
+    Private Sub btnRelactionCode_Click(sender As Object, e As EventArgs)
+        ValidateInputBoxArgs()
+    End Sub
+
+#End Region
+
+
+#Region "Method"
+
+    Private Sub ValidateEditBox()
+        ' This Method checks the input from a user in the EditBox
+        ' If the user input is greater than 8, an error is provided
+        ' If the user input is empty, an error is provided
+        ' If the user input is correct, an error would be provided
+        Dim rc As String = teRelationCode.Text
+        If rc = Nothing Then
+            DxErrorProvider1.SetError(teRelationCode, $"Relation Code field is empty", ErrorType.Warning)
+        ElseIf rc.Count > 8 Then
+            DxErrorProvider1.SetError(teRelationCode, $"Relation Code should have Max 8 character", ErrorType.Warning)
+        Else
+            DxErrorProvider1.SetError(teRelationCode, Nothing, ErrorType.None)
+        End If
+    End Sub
+
+    Private Sub ValidateInputBoxArgs()
+        ' This Method creates an InputBox, which the user can put in a new Relocation code
+        ' If the user input is greater than 8, an error is provided
+        ' If the user input is empty, the TextEdit won't be updated
+        ' If the user input is correct the TextEdit will inherit the user input
 
         ' Create Custom TextBox
         Dim editor As New TextEdit()
@@ -22,9 +63,12 @@ Public Class MessageBoxError
             ElseIf result.ToString.Count > 8 Then ' Show ErrorProveder
                 DxErrorProvider1.SetError(editor, $"Max 8 characters", ErrorType.Warning)
             ElseIf result.ToString.Count <= 8 Then ' Continue
+                teRelationCode.Text = result
                 Exit Sub
             End If
         End While
     End Sub
+
+#End Region
 
 End Class
